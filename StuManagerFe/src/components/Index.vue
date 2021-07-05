ee8cdb7631e96e4e6a567b830ff39897db1c59f7
zhangzhibang0309 <295854713@qq.com>
2021-07-05 12:56:22 +0800

    更新

--- StuManagerFe/src/components/Index.vue
+++ StuManagerFe/src/components/Index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>index</div>
+  <div >
+    <img  src="https://seopic.699pic.com/photo/40192/4801.jpg_wh1200.jpg" alt="">
+
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-
 export default {
   name: "Index",
   data() {
@@ -17,4 +19,8 @@ export default {
 
 
 <style scoped>
+img {
+  width: 100%;
+  height: 91.4vh;
+}
 </style>
--- StuManagerFe/src/components/Login.vue
+++ StuManagerFe/src/components/Login.vue
@@ -10,13 +10,51 @@
         <br />
         <br />
 
-        <label for="">username</label>
+        <!-- <label for="">username</label>
         <input type="text" v-model="loginData.name" />
         <br />
         <label for="">password</label>
-        <input type="text" v-model="loginData.password" />
-        {{loginData}}
-        <button @click="Login">登录</button>
+        <input type="text" v-model="loginData.password" /> -->
+
+        <el-form
+          :model="numberValidateForm"
+          ref="numberValidateForm"
+          label-width="80px"
+          class="demo-ruleForm"
+        >
+          <el-form-item
+            label="用户名"
+            prop="name"
+            :rules="[{ required: true, message: '用户名不能为空' }]"
+          >
+            <el-input
+              type="name"
+              v-model.number="numberValidateForm.name"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item
+            label="密码"
+            prop="password"
+            :rules="[{ required: true, message: '用户名不能为空' }]"
+          >
+            <el-input
+              type="password"
+              v-model="numberValidateForm.password"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button id="btn" type="primary" @click="Login"
+              >提交</el-button
+            >
+          </el-form-item>
+        </el-form>
+
+        <!-- <div id="btn">
+          <el-button @click="Login" type="primary">主要按钮</el-button>
+        </div> -->
       </div>
     </div>
   </div>
@@ -33,14 +71,18 @@ export default {
         name: "",
         password: "",
       },
+      numberValidateForm: {
+        name: "",
+        password: "",
+      },
     };
   },
   methods: {
     Login() {
       axios
-        .post("http://localhost:1123/login", this.loginData)
+        .post("http://localhost:1123/login", this.numberValidateForm)
         .then((res) => {
-          this.$router.push('/index')
+          this.$router.push("/index");
         })
         .catch((err) => {});
     },
@@ -54,7 +96,7 @@ export default {
 #background {
   position: absolute;
   z-index: 9998;
-  background-image: url("https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/7cff224agy1gqs5jqd242j214e0r5td9.jpg");
+  background-image: url("https://z3.ax1x.com/2021/07/05/RhN5Fg.jpg");
   background-size: 100% 100%;
   width: 100%;
   height: 100%;
@@ -62,23 +104,32 @@ export default {
 }
 
 #login-box {
-  background-image: url(https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/20210704000651.png) !important;
+  /* background-image: url(https://z3.ax1x.com/2021/07/05/RhwzHx.jpg) !important; */
   background-size: 100% !important;
   border-radius: 4%;
   background-position: 10% 60% !important;
   width: 30%;
-  height: 30%;
-  background: white;
+  height: 38%;
+  background: rgb(255, 255, 255, 0.2);
   margin: 0 auto;
   position: relative;
-  top: 30%;
+  top: 26%;
 }
 
 #title {
-  width: 16%;
+  width: 25%;
   margin: 0 auto;
 }
 .el-form-item__label {
   color: white !important;
+}
+
+#btn {
+  margin-left: 29%;
+}
+
+.el-input {
+  width: 300px;
+  margin-bottom: 3px;
 }
 </style>
